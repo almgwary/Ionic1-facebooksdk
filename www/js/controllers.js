6633a9c020aa53f469be058a6e2043e12f0b56e2
almgwary <almgwaryfci@gmail.com>
2017-09-04 18:04:23 +0200

    run facebook sdk

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,6 +1,51 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope) {})
+.controller('DashCtrl', function($scope) {
+
+
+  $scope.isLoggedIn =  false  ;
+  $scope.data =  {}  ;
+  var me = $scope ;
+
+  $scope.facebookLogin = function () {
+
+    if(facebookConnectPlugin) {
+      facebookConnectPlugin.login([], function (res) {
+        console.log('done log in', res);
+        me.isLoggedIn = true;
+        me.data = res;
+
+
+      }, function (e) {
+        console.log('error log in', e);
+        me.isLoggedIn = false;
+      });
+
+    }else {
+      console.error('facebookConnectPlugin not available please run in device ')
+    }
+
+  };
+
+
+  $scope.facebookLogOut = function () {
+    if(facebookConnectPlugin){
+         facebookConnectPlugin.logout(
+           function (res) {
+           console.log('done log out',res)
+           me.isLoggedIn =  false  ;
+             me.data =  {}  ;
+
+
+           }, function (e) {
+           console.log('error log out',e)
+             me.isLoggedIn =  false  ;
+         });
+    }else {
+      console.error('facebookConnectPlugin not available please run in device ')
+    }
+  }
+})
 
 .controller('ChatsCtrl', function($scope, Chats) {
   // With the new view caching in Ionic, Controllers are only called
